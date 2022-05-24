ae9244e55e9e40908ff5f64505e54c877f01b816
KOSSY <abonekosiso@gmail.com>
2022-05-24 08:37:32 +0100

    added

--- myApp.js
+++ myApp.js
@@ -1,36 +1,26 @@
+require('dotenv').config();
+const { MESSAGE_STYLE } = process.env
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
+
 
 console.log("Hello World")
 
+app.use('/public', express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+
+app.get('/json', (req,res) => {
+   res.json(MESSAGE_STYLE == "uppercase" ? {"message": "HELLO JSON"} : {"message": "Hello json"})
+})
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(__dirname + '/public')
 
 
 
